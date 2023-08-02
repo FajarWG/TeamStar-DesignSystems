322c28c59fd3356e214a1fe9116ad6023047611d
FajarWG <fajarwahyugumelar@gmail.com>
2023-08-03 00:47:43 +0700

    Update: Default storybook dark mode:bulb:

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,17 @@
 /** @type { import('@storybook/react').Preview } */
 
 import "../src/index.css";
+import { themes } from "@storybook/theming";
 
 const preview = {
   parameters: {
+    backgrounds: {
+      disable: true,
+    },
     actions: { argTypesRegex: "^on[A-Z].*" },
     docs: {
       controls: { exclude: ["style"] },
+      theme: themes.dark,
     },
     controls: {
       matchers: {
